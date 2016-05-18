1b8f7b623d73a889ab5743d8655cd0d090af30db
wangwangstar <1423247994@qq.com>
2016-05-18 09:40:47 +0800

    modified global.js

--- js/global.js
+++ js/global.js
@@ -20,5 +20,28 @@ function interAfter(newElement, targetElement) {
     }else {
         parent.insertBefore(newElement,targetElement,nextSibling);
     }
-
+}
+function addclass(element,value) {
+    if (!element.className){
+        element.className = value;
+    }else {
+        newClassName = element.className;
+        newClassName+= "";
+        newClassName+= value;
+        element.className = newClassName;
+    }
+}
+//取得导航列表中所有的链接，循环遍历这些链接
+function highLightPage() {
+    if (!document.getElementsByTagName)return false;
+    if (!document.getElementById)return false;
+    var headers =document.getElementsByTagName('header');
+    if (headers.length == 0)return false;
+    var navs = headers[0].getElementsByTagName('nav');
+    if(navs.length == 0)return false;
+    var links = navs[0].getElementsByTagName('a');
+    var linkurl;
+    for (var i=0;i<links.length;i++){
+        
+    }
 }
