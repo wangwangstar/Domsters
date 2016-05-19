2d2a9199b3b62c497652e82f51e57edfd2d18a82
wangwangstar <1423247994@qq.com>
2016-05-19 22:55:21 +0800

    modified index.html

--- js/global.js
+++ js/global.js
@@ -52,7 +52,7 @@ function highLightPage() {
         }
     }
 }
-addLoadEvent(highLightPage);
+
 // Home
 
 function moveElement(elementID,final_x,final_y,interval) {
@@ -102,11 +102,71 @@ function prepareSlideshow() {
     var intro = document.getElementById("intro");
     var slideshow = document.createElement("div");
     slideshow.setAttribute("id", "slideshow");
+    var frame = document.createElement("img");
+    frame.setAttribute("src","../img/frame.gif");
+    frame.setAttribute("alt","");
+    frame.setAttribute("id","frame");
+    slideshow.appendChild(frame);
     var preview = document.createElement("img");
     preview.setAttribute("src", "../img/slideshow.gif");
     preview.setAttribute("alt", "a glimpse of what awaits you");
     preview.setAttribute("id", "preview");
     slideshow.appendChild(preview);
     insertAfter(slideshow, intro);
-    var links =intro.getElementsByTagName("a");
+    var links = document.getElementsByTagName("a");
+    // var links =intro.getElementsByTagName("a");
+    var destination;
+    for (var i=0;i<links.length;i++){
+        links[i].onmouseover = function () {
+            destination = this.getAttribute("href");
+            if(destination.indexOf("index.html") != -1){
+                moveElement("preview",0,0,5);
+            }
+            if(destination.indexOf("about.html") != -1){
+                moveElement("preview",-150,0,5);
+            }
+            if(destination.indexOf("photos.html") != -1){
+                moveElement("preview",-300,0,5);
+            }
+            if(destination.indexOf("live.html") != -1){
+                moveElement("preview",-450,0,5);
+            }
+            if(destination.indexOf("contact.html") != -1){
+                moveElement("preview",-600,0,5);
+            }
+        }
+    }
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload !='function'){
+        window.onload = func;
+    }else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareSlideshow);
+addLoadEvent(highLightPage);
+
+
+function loadEvents() {
+    // home
+    prepareSlideshow();
+    // about
+    prepareInternalnav();
+    // photos
+    preparePlaceholder();
+    prepareGallery();
+    // live
+    stripeTables();
+    highlightRows();
+    displayAbbreviations();
+    // contact
+    focusLabels();
+    prepareForms();
 }
